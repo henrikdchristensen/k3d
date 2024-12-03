71cf07ffd62722d6920d466dc6f328daf152e841
henrikdchristensen <djl587@alumni.ku.dk>
2024-12-03 21:33:22 +0100

    fix

--- src/main/tests/test_comm.py
+++ src/main/tests/test_comm.py
@@ -1,6 +1,16 @@
 import requests
 
-def test_comm():
+def test_comm1():
     # Use the Kubernetes DNS name for the user service
-    response = requests.get('http://service-user.project.svc.cluster.local')
+    response = requests.get('http://service-user')
+    assert response.status_code == 200
+
+def test_comm2():
+    # Use the Kubernetes DNS name for the user service
+    response = requests.get('http://service-user:8082')
+    assert response.status_code == 200
+
+def test_comm3():
+    # Use the Kubernetes DNS name for the user service
+    response = requests.get('localhost/user') # Access from another service: http://service-user.project.svc.cluster.local
     assert response.status_code == 200
